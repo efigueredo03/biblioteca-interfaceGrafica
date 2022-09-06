9bf20e6dcdfd786ef14bd9c3acc95224ccce5112
Emanoel Bezerra Figueredo <efigueredo@lyra-network.com.br>
2022-09-06 16:33:56 -0300

    save project

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.io.IOException;
+
+import br.com.biblioteca.entiade.usuario.TipoUsuario;
+import br.com.biblioteca.entiade.usuario.UsuarioFisico;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 
 public class LoginController {
 	@FXML
@@ -21,11 +26,48 @@ public class LoginController {
 	// Event Listener on Button[#bt_entrar].onAction
 	@FXML
 	public void botaoEntrarPressionado(ActionEvent event) {
-		System.out.println("entrar");
+		this.logar();
 	}
 	// Event Listener on Button[#bt_cadastrar].onAction
 	@FXML
-	public void botaoCadatrarPressionado(ActionEvent event) {
-		System.out.println("logar");
+	public void botaoCadatrarPressionado(ActionEvent event) throws IOException {
+		this.abrirJanelaDeCadastro();
 	}
+	
+	private void abrirJanelaDeCadastro() throws IOException {
+		Main main = new Main();
+		main.changeScene("FXMLCadastrar.fxml");
+	}
+	
+	private void logar() {
+		String usuario = this.tf_usuario.getText();
+		String senha = this.tf_senha.getText();
+		
+		UsuarioFisico ObjetoUsuario = new UsuarioFisico(usuario, senha, null, null, null, null, null, null);
+				
+		
+		if(this.verificarEntradaDeDados(ObjetoUsuario)) {
+			return;
+		}
+		
+		PacoteParaServidor pacote = new PacoteParaServidor()
+		
+	}
+	
+	private void trocarMensagemLabelInformacao(String mensagem) {
+		this.lb_status.setText(mensagem);
+	}
+	
+	private boolean verificarEntradaDeDados(UsuarioFisico usuario) {
+		if (usuario.getId().length() == 0 && usuario.getSenha().length() == 0) {
+			this.trocarMensagemLabelInformacao("Preencha todos os campos");
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean verificarSeUsuarioExisteNoSistema() {
+		
+	}
+	
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		stg = primaryStage;
-		Parent rootLogin = FXMLLoader.load(getClass().getResource("FXMLCadastrar.fxml"));
+		Parent rootLogin = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
 		primaryStage.setTitle("Login");
 		primaryStage.setScene(new Scene(rootLogin));
 		primaryStage.show();
